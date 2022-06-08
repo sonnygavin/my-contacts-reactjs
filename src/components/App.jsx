7578b2792435f7fb7e1ec14b1b1c8372fde44e25
Gavin Waako <102330724+sonnygavin@users.noreply.github.com>
2022-06-08 12:55:33 +0200

    Use map function to render screen componentS

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,28 @@ import Card from "./Card";
 import contacts from "../contacts";
 import Avatar from "./Avatar";
 
+function createCard(contact) {
+  return (
+    <Card
+      key={contacts.id}
+      id={contacts.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Avatar img="https://yt3.ggpht.com/ytc/AKedOLT62zgYTvNecY2F3IJB1_NWycr7Sp2sfPjCF9_G=s900-c-k-c0x00ffffff-no-rj" />
-      <Card
+
+      {contacts.map(createCard)}
+
+      {/* <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -25,7 +41,7 @@ function App() {
         img={contacts[2].imgURL}
         tel={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
